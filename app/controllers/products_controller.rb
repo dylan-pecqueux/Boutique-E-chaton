ced7542825895a9625573ed1cecc1d544da76879
Texier <julestexier@live.fr>
2020-11-24 13:16:31 +0100

    page show et footer

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
     def index
         @products = Product.all
     end
+
+    def show
+        @product = Product.find(params[:id])
+        @products = Product.all
+    end
 end
