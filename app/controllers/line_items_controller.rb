3c2e8eea28138241bfb09d858531e6066643550f
stephen2790022 <toulouse.stephen1@gmail.com>
2020-11-25 14:10:53 +0100

    LINE_ITEM Controller ALL redirect_to + ajout des images des produit dans le cartre

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -12,7 +12,7 @@ class LineItemsController < ApplicationController
         @line_item = @cart.add_product(product)
         
         if @line_item.save 
-            redirect_to @cart, success: "vous avez ajouté #{product.title} avec succès"
+            redirect_to '/', success: "vous avez ajouté #{product.title} avec succès"
         else
             redirect_to '/'
         end
@@ -21,6 +21,6 @@ class LineItemsController < ApplicationController
     def destroy
         @line_item = LineItem.find(params[:id])
         @line_item.destroy
-        redirect_to '/', info: "Vous avez supprimer votre évènement avec succès"
+        redirect_to @line_item.cart, info: "Vous avez supprimer votre produit avec succès"
     end
 end
