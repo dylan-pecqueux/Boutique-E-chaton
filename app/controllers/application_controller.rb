4bdf6a8928c0d59b96814fa0b329cd64462ef55d
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2020-11-25 05:23:30 +0100

    test heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
 	add_flash_types :info, :error, :warning, :success, :notice, :danger
 
-	before_action :configure_devise_parameters, if: :devise_controller?
+  before_action :configure_devise_parameters, if: :devise_controller?
+  skip_before_action :verify_authenticity_token
 
     def configure_devise_parameters
 			devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :is_alive, :email, :password, :avatar, :password_confirmation)}
