55482133379f0dadfd3e7be1855c99c9f5a589fc
Youcef <cefyou28@gmail.com>
2020-11-26 18:58:22 +0100

    gestion de plusieurs cart et historique

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -1,7 +1,8 @@
 module CurrentCart
     def set_cart
         if user_signed_in?
-            @cart = Cart.find_or_create_by(user_id: current_user.id) 
+            @cart = Cart.find_or_create_by(user_id: current_user.id)
+            @cart = Cart.last
         end            
     end
 end
